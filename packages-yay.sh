fdc11dce3e1c039d38cb4259256baea175788949
prothegee <prothegee@proton.me>
2026-02-22 17:19:15 +0700

    list of pacman yay

---
+++ packages-yay.sh
@@ -0,0 +1,12 @@
+yay -Syu \
+    basedpyright-bin \
+    flamegraph \
+    jdtls \
+    kotlin-lsp-bin \
+    obs-cli-debug \
+    obs-cmd \
+    obs-cmd-debug \
+    sqls \
+    squiid \
+    wrk \
+    wscat ;
